7710bb113d60c997dec74a432518e34c05cd1f2f
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-09-13 23:12:46 +0400

    converting json to csv

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,25 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: csvtojson <csv_file_path> <json_file_path>")
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: csvtojson <conversion_direction> <input_file_path> <output_file_path>")
 		return
 	}
-	csvFilePath := os.Args[1]
-	jsonFilePath := os.Args[2]
 
-	err := pkg.ConvertCSVToJSON(csvFilePath, jsonFilePath)
+	conversionDirection := os.Args[1]
+	inputFilePath := os.Args[2]
+	outputFilePath := os.Args[3]
+
+	var err error
+	if conversionDirection == "csvtojson" {
+		err = pkg.ConvertCSVToJSON(inputFilePath, outputFilePath)
+	} else if conversionDirection == "jsontocsv" {
+		err = pkg.ConvertJSONToCSV(inputFilePath, outputFilePath)
+	} else {
+		fmt.Println("Unknown conversion direction. Use 'csvtojson' or 'jsontocsv'.")
+		return
+	}
+
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
--- pkg/converter.go
+++ pkg/converter.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
@@ -44,6 +45,55 @@ func ConvertCSVToJSON(csvFilePath string, jsonFilePath string) error {
 	err = jsonEncoder.Encode(jsonData)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func ConvertJSONToCSV(jsonFilePath string, csvFilePath string) error {
+	jsonFile, err := os.Open(jsonFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+
+	var jsonData []map[string]interface{}
+
+	jsonDecoder := json.NewDecoder(jsonFile)
+	err = jsonDecoder.Decode(&jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvFile, err := os.Create(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+
+	headers := make([]string, 0)
+	for header := range jsonData[0] {
+		headers = append(headers, header)
+	}
+	err = writer.Write(headers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range jsonData {
+		var row []string
+		for _, header := range headers {
+			value := entry[header]
+			strValue := fmt.Sprintf("%v", value)
+			row = append(row, strValue)
+		}
+		err = writer.Write(row)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	return nil
 }
