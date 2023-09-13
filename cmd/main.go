cc16539199625668d6f15c8b2e8d4cd98894a104
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-09-13 22:51:58 +0400

    converting cav to json

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/GiorgiMakharadze/csv-json-converter-CLI-golang/pkg"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: csvtojson <csv_file_path> <json_file_path>")
+		return
+	}
+	csvFilePath := os.Args[1]
+	jsonFilePath := os.Args[2]
+
+	err := pkg.ConvertCSVToJSON(csvFilePath, jsonFilePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Conversion successful!")
+}
--- pkg/converter.go
+++ pkg/converter.go
@@ -1,2 +1,49 @@
 package pkg
 
+import (
+	"encoding/csv"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+func ConvertCSVToJSON(csvFilePath string, jsonFilePath string) error {
+	csvFile, err := os.Open(csvFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jsonData []map[string]string
+
+	headers := csvData[0]
+
+	for _, row := range csvData[1:] {
+		entry := make(map[string]string)
+		for i, header := range headers {
+			entry[header] = row[i]
+		}
+		jsonData = append(jsonData, entry)
+	}
+
+	jsonFile, err := os.Create(jsonFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+
+	jsonEncoder := json.NewEncoder(jsonFile)
+	err = jsonEncoder.Encode(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
